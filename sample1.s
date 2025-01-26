f4fc339243ba2f1edd4c195bdf7fcfad44110aed
Mrz <mohammadreza.iph@gmail.com>
2025-01-26 14:40:03 +0330

    Auto-commit from Makefile

--- sample1.s
+++ sample1.s
@@ -5,14 +5,14 @@ msg:    .asciz "Hello, ARM on Termux!\n"
 
 .section .text
 _start:
-    # نوشتن پیام به خروجی (stdout)
-    mov x0, #1          @ File descriptor 1 (stdout)
-    ldr x1, =msg        @ بارگذاری آدرس پیام به x1
-    ldr x2, =21         @ طول پیام "Hello, ARM on Termux!\n" (21 بایت)
-    mov x8, #64         @ شماره syscall برای sys_write (64 در AArch64)
-    svc #0              @ فراخوانی syscall برای نوشتن پیام
+    
+    mov x0, #1          
+    ldr x1, =msg        
+    ldr x2, =21         
+    mov x8, #64         
+    svc #0              
 
-    # پایان دادن به برنامه
-    mov x0, #0          @ وضعیت خروجی 0
-    mov x8, #93         @ شماره syscall برای sys_exit (93 در AArch64)
-    svc #0              @ فراخوانی syscall برای خروج          
+    
+    mov x0, #0          
+    mov x8, #93         
+    svc #0
