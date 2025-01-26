aad07095b0e834c888afbdaab41c9485808ffa7d
Mrz <mohammadreza.iph@gmail.com>
2025-01-26 14:48:26 +0330

    Auto-commit from Makefile

--- sample1.s
+++ sample1.s
@@ -5,14 +5,7 @@ msg:    .asciz "Hello, ARM on Termux!\n"
 
 .section .text
 main:
-    
-    mov x0, #1          
-    ldr x1, =msg        
-    ldr x2, =21         
-    mov x8, #64         
-    svc #0              
-
-    
+             
     mov x0, #0          
     mov x8, #93         
     svc #0
